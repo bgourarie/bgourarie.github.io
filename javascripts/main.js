b4f41b8ccb69d241114c36aea03951c37f215cfe
Ben Gourarie <bgourarie@uptopcorp.com>
2016-05-30 19:24:01 -0700

    Lots of minor css/js adjustments squashed together to make it seem prettier(+15 squashed commits) Squashed commits: [82f4870] fixing a dollar sign [8ed066b] getting simple instead of fancy [9bf8f59] is it pound or hashtag? Will we ever know? [7b1c6d6] fixing div hierarchy [a302a0c] changing section to div because apparently [eaec0e0] hiding projects instea of using display none [49abda1] fixing active class adding [ebe2e9d] fixing bootstrap errors [76bb771] adding some js to toggle active menu items and show content maybe [cb5fa8e] more minor adjustments to my logo. I should probbably dev this locally. [2313d05] making my name smaller because it was embarassingly large. [35ff4f0] adding some jquery action [6ca0e76] I AM NOT A FRONT END DEV . Moar CSS changes. [9193043] removing some unused css that I realized i dont like [b8310ac] adding JQuery and redoing the bootstrap nav a bit, adding content to about

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,19 @@
-console.log('This would be the main JS file.');
+$( document ).ready(function() {
+
+		// hide stuff we dont want visible...
+		$('#projects').hide();
+
+ 		$('.menu-item-projects').on('click', function(e){
+ 			$('li').removeClass('active');
+ 			$(this).addClass('active');
+			$('#about').hide();
+ 			$('#projects').show();
+ 		});
+	 	$('.menu-item-about').on('click', function(e){
+	 		$('li').removeClass('active');
+	 		$(this).addClass('active');
+			$('#projects').hide();
+	 		$('#about').show();
+	 	});
+
+});
